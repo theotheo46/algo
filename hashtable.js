c341e2c1b3c5cf459930ae9306a37b8a27f75a78
Dmitry <theotheo46@gmail.com>
2023-03-11 21:03:11 +0300

    111

--- hashtable.js
+++ hashtable.js
@@ -1,51 +1,60 @@
 class HashTable {
-    constructor(size) {
-      if (!size || size < 0) {
-          throw new Error('Размер должен быть положительным числом');
-      }
-  
-      this.size = size;
-      this.memory = new Array(size);
+  constructor(size) {
+    if (!size || size < 0) {
+        throw new Error('Размер должен быть положительным числом');
     }
-      
-      // Добавляет пару [key, value] в хеш-таблицу.
-      // Если ключ существует, перезаписывает значение.
-    set(key, value) {
-   
-    }
-      
-      // Возвращает значение в хеш-таблице по ключу.
-      // Если ключа нет, возвращает undefined.
-    get(key) {
-      
-    }
-      
-      // Удаляет значение из хеш-таблице по ключу.
-    remove(key) {
-    
-    }
+
+    this.size = size;
+    this.memory = new Array();
   }
-  
-  // Хеширующая функция.
-  function hash(key, size) {
-    const MAX_LENGTH = 200;
-  
-    const start = key.length > MAX_LENGTH ? 
-          Math.floor((key.length % MAX_LENGTH) / 2) : 0;
-    const end = Math.min(key.length, MAX_LENGTH);
-  
-    let total = 0;
-  
-    for (let i = 0; i < end; i++) {
-        const charCode = key.charCodeAt(start + i);
-        total = (total + charCode * (i + 1)) % size;
+	
+	// Добавляет пару [key, value] в хеш-таблицу.
+	// Если ключ существует, перезаписывает значение.
+  set(key, value) {
+    let memoryLocation = this.hash(key, this.size);
+    if (!this.memory[memoryLocation]) {
+      this.memory[memoryLocation] = [];
     }
-  
-    return total;
+    let index = this.memory[memoryLocation].find((x) => x[0] === key);
+    if (index !== undefined) {
+      this.memory[memoryLocation][index] = value;
+    }
+    else {
+       this.memory[memoryLocation].push([key, value]);
+    }
+    //return this.memory;
+  }
+	
+	// Возвращает значение в хеш-таблице по ключу.
+	// Если ключа нет, возвращает undefined.
+  get(key) {
+    let memoryLocation = this.hash(key);
+    if (!this.table[memoryLocation]) return null;
+    return this.memory[memoryLocation].find((x) => x[0] === key)[1];
+  }
+	
+	// Удаляет значение из хеш-таблице по ключу.
+  remove(key) {
+    let memoryLocation = this.hash(key);
+    if (!this.table[memoryLocation]) return null;
+    this.memory[memoryLocation] = [];
+  }
+}
+
+// Хеширующая функция.
+function hash(key, size) {
+  const MAX_LENGTH = 200;
+
+  const start = key.length > MAX_LENGTH ? 
+		Math.floor((key.length % MAX_LENGTH) / 2) : 0;
+  const end = Math.min(key.length, MAX_LENGTH);
+
+  let total = 0;
+
+  for (let i = 0; i < end; i++) {
+      const charCode = key.charCodeAt(start + i);
+      total = (total + charCode * (i + 1)) % size;
   }
 
-
-  const arr = new Array();
-  console.log(arr)
-  arr[22] = '22'
-  console.log(arr)
+  return total;
+}
