29f3c3c2226810c58f28a7dded7c92ba041aeb35
Dmitry <theotheo46@gmail.com>
2023-03-12 00:06:17 +0300

    111

--- hashtable.js
+++ hashtable.js
@@ -11,33 +11,40 @@ class HashTable {
 	// Добавляет пару [key, value] в хеш-таблицу.
 	// Если ключ существует, перезаписывает значение.
   set(key, value) {
-    let memoryLocation = this.hash(key, this.size);
+    let memoryLocation = hash(key, this.size);
     if (!this.memory[memoryLocation]) {
       this.memory[memoryLocation] = [];
     }
-    let index = this.memory[memoryLocation].find((x) => x[0] === key);
-    if (index !== undefined) {
-      this.memory[memoryLocation][index] = value;
+    for (let i = 0; i < this.memory[memoryLocation].length; i++) {
+      if (this.memory[memoryLocation][i][0] === key) {
+         this.memory[memoryLocation][i][1] = value;
+         return;
+      }
     }
-    else {
-       this.memory[memoryLocation].push([key, value]);
-    }
-    //return this.memory;
+    this.memory[memoryLocation].push([key, value]);
   }
 	
 	// Возвращает значение в хеш-таблице по ключу.
 	// Если ключа нет, возвращает undefined.
   get(key) {
-    let memoryLocation = this.hash(key);
-    if (!this.table[memoryLocation]) return null;
+    let memoryLocation = hash(key, this.size);
+    console.log(memoryLocation);
+    if (!this.memory[memoryLocation]) return null;
     return this.memory[memoryLocation].find((x) => x[0] === key)[1];
   }
 	
 	// Удаляет значение из хеш-таблице по ключу.
   remove(key) {
-    let memoryLocation = this.hash(key);
-    if (!this.table[memoryLocation]) return null;
-    this.memory[memoryLocation] = [];
+    let memoryLocation = hash(key, this.size);
+    if (!this.memory[memoryLocation]) return null;
+    for (let i = 0; i < this.memory[memoryLocation].length; i++) {
+      if (this.memory[memoryLocation][i][0] === key) {
+         delete this.memory[memoryLocation][i][1];
+         this.memory[memoryLocation].splice(i, 1);
+         return true;
+      }
+   }
+   return false;
   }
 }
 
@@ -58,3 +65,16 @@ function hash(key, size) {
 
   return total;
 }
+
+const ht = new HashTable(100);
+
+console.log(ht);
+ht.set('abcde', 10);
+console.log(ht);
+ht.set('abcdee', 100);
+console.log(ht);
+console.log(ht.get('abcde'));
+ht.remove('abcde');
+console.log(ht);
+ht.remove('abcdee');
+console.log(ht);
