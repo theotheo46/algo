b8e5bcb11594ae9d40fe3b9cbc216a3c208877d9
Dmitry <theotheo46@gmail.com>
2023-03-24 23:11:40 +0300

    added

--- subarray.js
+++ subarray.js
@@ -3,23 +3,24 @@
 // ([1, 2, 5, 3], 3) -> 2
 // ([10, 5, 6, 7, 8, 7], 15) -> 3
 
-function subarraySum(nums, k) {
-    let max_ending_here = 0, max_so_far = Number.MIN_VALUE;
-              for (let i = 0; i < k; i++) {
-   
-                  // include current element to previous subarray only
-                  // when it can add to a bigger number than itself.
-                  if (arr[i] <= max_ending_here + arr[i]) {
-                      max_ending_here += nums[i];
-                  }
-   
-                  // Else start the max subarray from current element
-                  else {
-                      max_ending_here = nums[i];
-                  }
-                  if (max_ending_here > max_so_far) {
-                      max_so_far = max_ending_here;
-                  }
-              }
-              return max_so_far;
-  }
+function subarraySum(arr, sum) {
+    var len = arr.length;
+    var count = 0;
+ 
+    for(var i = 0; i < len; i++){
+       var n = 0;
+      
+       for(var j = i; j < len; j++){    
+          n += arr[j];
+       
+          if(n === sum) {
+            count++;
+            break;
+          }
+       }
+    }
+ 
+    return count;
+};
+
+ console.log(subarraySum([1, 2, 5, 3], 3));
