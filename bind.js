39f207d9e75771025c3ed1feef0593747f5513ad
Dmitry <theotheo46@gmail.com>
2023-03-27 18:35:03 +0300

    added

--- bind.js
+++ bind.js
@@ -1,23 +1,22 @@
-/**
-var F = function() {
-  console.log('foo is', this.foo);
-}
-var F1 = F.bind({ foo: 'bar' })
-
-F()               // foo is undefined
-F1()              // foo is bar
-
-var f = new F()   // foo is undefined
-var f1 = new F1() // foo is bar
-
-console.log(f instanceof F)    // true
-console.log(f1 instanceof F)   // false
-*/
 // не удаляйте следующую строчку, без нее проект не соберётся
 // eslint-disable-next-line
 Function.prototype.bind = function (context) {
-    const func = this;
+    // method is attached to the prototype, so just refer to it as this.
+    var func = this;
+    var previousArgs = [].slice.call(arguments, 1);
     return function () {
-        return func.apply(context);
+        var currentArgs = [].slice.call(arguments);
+        var combinedArgs = [].concat(previousArgs, currentArgs);
+        return func.apply(context, combinedArgs);
     };
 };
+var F = function () {
+    console.log('foo is', this.foo);
+};
+var F1 = F.bind({ foo: 'bar' });
+F(); // foo is undefined
+F1(); // foo is bar
+var f = new F(); // foo is undefined
+var f1 = new F1(); // foo is bar
+console.log(f instanceof F); // true
+console.log(f1 instanceof F); // false 
--- bind.ts
+++ bind.ts
@@ -1,4 +1,20 @@
-/**
+
+
+// не удаляйте следующую строчку, без нее проект не соберётся
+// eslint-disable-next-line
+Function.prototype.bind = function(context : unknown) {  
+  // method is attached to the prototype, so just refer to it as this.
+  let func = this;
+  let previousArgs = [].slice.call(arguments, 1);
+
+  return function(){
+    let currentArgs = [].slice.call(arguments);
+    let combinedArgs = [].concat(previousArgs, currentArgs);
+    return func.apply(context, combinedArgs);
+  };
+};
+
+
 var F = function() {
   console.log('foo is', this.foo);
 }
@@ -11,18 +27,7 @@ var f = new F()   // foo is undefined
 var f1 = new F1() // foo is bar
 
 console.log(f instanceof F)    // true
-console.log(f1 instanceof F)   // false
-*/
+console.log(f1 instanceof F)   // false 
 
-// не удаляйте следующую строчку, без нее проект не соберётся
-// eslint-disable-next-line
-Function.prototype.bind = function(context: unknown, ...args)  {
-    let func = this;
-    // eslint-disable-next-line
-    return function (...newArgs) {
-      func.apply(context, [...args, ...newArgs]);
-    };
-  };
 
-           // foo is bar
   
