968b7e693ee88b9ef6cc4dce041980261fe94b32
Dmitry <theotheo46@gmail.com>
2023-04-25 15:46:41 +0300

    added

---
+++ throttle.js
@@ -0,0 +1,43 @@
+const someCalc = function(a) {
+  console.log(a + this.b)
+};
+
+
+function throttle(callback, delay, context = this) {
+
+  let isThrottled = false,
+    savedArgs,
+    savedThis;
+
+  function wrapper() {
+
+    if (isThrottled) { // (2)
+      savedArgs = arguments;
+      savedThis = context;
+      return;
+    }
+
+    callback.apply(context, arguments); // (1)
+
+    isThrottled = true;
+
+    setTimeout(function() {
+      isThrottled = false; // (3)
+      if (savedArgs) {
+        wrapper.apply(savedThis, savedArgs);
+        savedArgs = savedThis = null;
+      }
+    }, delay);
+  }
+
+  return wrapper;
+}
+
+// затормозить функцию до одного раза в 1000 мс
+const f1000 = throttle(someCalc, 1000, {b: ' call'});
+f1000(1); // выведет 1 call
+f1000(2); // (тормозим, не прошло 1000 мс)
+f1000(3); // (тормозим, не прошло 1000 мс)
+
+// ко/т 1000 мс...
+// выведет 3 call, промежуточное значение 2 call игнорируется
